d1a8297057d1d81803a788012841e553c815cee3
Parthvi Vala <parthvi.vala@gmail.com>
2024-02-05 20:12:04 +0530

    Add LICENSE (#5)
    * Create LICENSE
    
    * Add license to all the test files
    
    * 2022 -> 2023

--- hosted/eks/support_matrix/support_matrix_suite_test.go
+++ hosted/eks/support_matrix/support_matrix_suite_test.go
@@ -1,3 +1,17 @@
+/*
+Copyright © 2023 - 2024 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package support_matrix_test
 
 import (
@@ -6,8 +20,9 @@ import (
 
 	"testing"
 
-	"github.com/rancher/hosted-providers-e2e/hosted/helpers"
 	"github.com/rancher/shepherd/extensions/clusters/kubernetesversions"
+
+	"github.com/rancher/hosted-providers-e2e/hosted/helpers"
 )
 
 var (
--- hosted/gke/p0/p0_importing_test.go
+++ hosted/gke/p0/p0_importing_test.go
@@ -1,17 +1,32 @@
+/*
+Copyright © 2023 - 2024 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package p0_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/rancher/hosted-providers-e2e/hosted/gke/helper"
-	"github.com/rancher/hosted-providers-e2e/hosted/helpers"
 	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
 	"github.com/rancher/shepherd/extensions/clusters"
 	"github.com/rancher/shepherd/extensions/clusters/gke"
 	nodestat "github.com/rancher/shepherd/extensions/nodes"
 	"github.com/rancher/shepherd/extensions/workloads/pods"
 	"github.com/rancher/shepherd/pkg/config"
+
+	"github.com/rancher/hosted-providers-e2e/hosted/gke/helper"
+	"github.com/rancher/hosted-providers-e2e/hosted/helpers"
 )
 
 var _ = Describe("P0Importing", func() {
